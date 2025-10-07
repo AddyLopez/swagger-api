2118f11d1212b9434543f0dc9ca44bf75ae8c0af
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-10-07 15:23:40 -0600

    Complete POST operation logic

--- orders_project_starting/server.js
+++ orders_project_starting/server.js
@@ -16,7 +16,12 @@ server.get('/orders', (request, response) => {
 });
 
 //Add the /neworder code here!
-
+server.post('/neworder', express.json(), (request, response) => {
+      orderData.orders.push(request.body);
+      fs.writeFileSync('orders.json', JSON.stringify(orderData));
+      response.send("Success");
+      console.log("Success");
+});
 
 //Add the /update/:id code here!
 
